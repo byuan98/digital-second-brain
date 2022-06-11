868fff4bb43374d664d12a779db78fdbadacc796
ZhangBoyuan <byuan98@outlook.com>
2022-06-11 17:38:08 +0800

    feat: 新增dto对象及相关枚举

---
+++ dsb-common/src/main/java/pers/boyuan/common/constants/ResponseEnum.java
@@ -0,0 +1,28 @@
+package pers.boyuan.common.constants;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
+/**
+ * @author ZhangBoyuan
+ * @date 2022-06-11
+ */
+@Getter
+@AllArgsConstructor
+public enum ResponseEnum {
+
+    /**
+     * 0 -> 操作成功
+     * */
+    SUCCESS(0, "操作成功"),
+    /**
+     * 1 -> 操作失败, 请稍后重试
+     * */
+    FAIL(1, "操作失败，请稍后重试"),
+    ;
+
+    private Integer code;
+
+    private String message;
+}
