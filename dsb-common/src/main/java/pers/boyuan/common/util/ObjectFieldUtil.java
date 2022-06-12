463e17ce5ae2752d8402597fa34d21924ed96510
ZhangBoyuan <byuan98@outlook.com>
2022-06-12 17:54:23 +0800

    feat: 新增对象字段工具类

---
+++ dsb-common/src/main/java/pers/boyuan/common/util/ObjectFieldUtil.java
@@ -0,0 +1,69 @@
+package pers.boyuan.common.util;
+
+import java.lang.reflect.Field;
+import java.util.Objects;
+
+/**
+ * 对象属性工具类
+ *
+ * @author ZhangBoyuan
+ * @date 2022-06-12
+ */
+public class ObjectFieldUtil {
+
+    /**
+     * 判断对象属性是否全部为null
+     */
+    public static Boolean allNull(Object object) {
+        if (Objects.isNull(object)) {
+            return Boolean.TRUE;
+        }
+
+        Integer nullCount = 0;
+
+        Field[] fieldArray = object.getClass().getDeclaredFields();
+        for (Field field : fieldArray) {
+            field.setAccessible(Boolean.TRUE);
+            try {
+                if (Objects.isNull(field.get(object))) {
+                    nullCount++;
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return nullCount.equals(fieldArray.length) ? Boolean.TRUE : Boolean.FALSE;
+    }
+
+    /**
+     * 判断对象属性是否全部非null
+     */
+    public static Boolean allNonNull(Object object) {
+        return allNull(object) ? Boolean.FALSE : Boolean.TRUE;
+    }
+
+    /**
+     * 判断对象属性之一是否存在null
+     */
+    public static Boolean existNull(Object object) {
+        if (Objects.isNull(object)) {
+            return Boolean.TRUE;
+        }
+
+        Field[] fieldArray = object.getClass().getDeclaredFields();
+        for (Field field : fieldArray) {
+            field.setAccessible(Boolean.TRUE);
+            try {
+                if (Objects.isNull(field.get(object))) {
+                    return Boolean.TRUE;
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return Boolean.FALSE;
+    }
+
+}
