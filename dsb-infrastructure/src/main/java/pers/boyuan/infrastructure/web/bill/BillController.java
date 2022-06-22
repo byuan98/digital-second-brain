82937b94c143f3b43a0b5623e86b2b5f57e5e82e
ZhangBoyuan <byuan98@outlook.com>
2022-06-22 20:49:15 +0800

    feat: 账单表相关接口预留

--- dsb-infrastructure/src/main/java/pers/boyuan/infrastructure/web/bill/BillController.java
+++ dsb-infrastructure/src/main/java/pers/boyuan/infrastructure/web/bill/BillController.java
@@ -1,8 +1,14 @@
 package pers.boyuan.infrastructure.web.bill;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pers.boyuan.application.bill.BillAppService;
 import pers.boyuan.common.dto.Response;
 
 /**
@@ -13,24 +19,38 @@ import pers.boyuan.common.dto.Response;
  */
 @Controller
 @RequestMapping("/biz/bill")
+@Api(tags = "消费账单表相关接口")
 public class BillController {
 
+    @Autowired
+    private BillAppService billAppService;
+
+    @PostMapping("/create")
+    @ApiOperation("创建账单表数据")
     public Response create() {
         return null;
     }
 
+    @PostMapping("/delete")
+    @ApiOperation("根据条件逻辑删除账单表数据")
     public Response delete() {
         return null;
     }
 
+    @PostMapping("update")
+    @ApiOperation("更新账单表数据")
     public Response update() {
         return null;
     }
 
+    @PostMapping("/query")
+    @ApiOperation("查询账单表数据")
     public Response query() {
         return null;
     }
 
+    @GetMapping("exportExcel")
+    @ApiOperation("根据指定条件导出账单表数据为excel")
     public void exportExcel() {
         return;
     }
