a420ef3bb263ece7f8be9d76c64f6d11541f6b7b
ZhangBoyuan <byuan98@outlook.com>
2022-06-22 09:27:37 +0800

    feat: 字符串解压工具类变量名规范化

--- dsb-common/src/main/java/pers/boyuan/common/util/StringUnzipUtil.java
+++ dsb-common/src/main/java/pers/boyuan/common/util/StringUnzipUtil.java
@@ -13,10 +13,13 @@ import java.util.zip.GZIPInputStream;
  */
 public class StringUnzipUtil {
 
+    /**
+     * 解压gzip格式字符串
+     */
     public static String gzip(String str) {
         try {
-            byte[] test = str.getBytes(StandardCharsets.ISO_8859_1);
-            InputStream inputStream = new ByteArrayInputStream(test);
+            byte[] byteArray = str.getBytes(StandardCharsets.ISO_8859_1);
+            InputStream inputStream = new ByteArrayInputStream(byteArray);
             GZIPInputStream gzipInputStream = new GZIPInputStream(inputStream);
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(gzipInputStream));
             String result = bufferedReader
