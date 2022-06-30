d510baebff7b4d35fefa3c52c1a69079241572dc
ZhangBoyuan <byuan98@outlook.com>
2022-06-30 14:47:11 +0800

    feat: 新增字典表类型常量类，维护字典表type

---
+++ dsb-common/src/main/java/pers/boyuan/common/constants/DictionaryTypeConstant.java
@@ -0,0 +1,15 @@
+package pers.boyuan.common.constants;
+
+/**
+ * 字典表类型常量类
+ *
+ * @author ZhangBoyuan
+ * @date 2022-06-30
+ */
+public class DictionaryTypeConstant {
+    /**
+     * 账单类型字典表type
+     */
+    public static final String BILL_CATEGORY = "bill_category";
+
+}
