07e549b9dff05e66f9e3c044a6a3b060b169f265
ZhangBoyuan <byuan98@outlook.com>
2022-06-30 14:50:15 +0800

    docs: 字典表初始值sql维护

---
+++ sql/basic/update.sql
@@ -0,0 +1,11 @@
+INSERT INTO `data_dictionary`(`type`, `code`, `name`, `remark`)
+VALUES ('bill_category', '0', '餐饮', '账单分类'),
+       ('bill_category', '1', '交通', '账单分类'),
+       ('bill_category', '2', '书籍', '账单分类'),
+       ('bill_category', '3', '礼金', '账单分类'),
+       ('bill_category', '4', '住房', '账单分类'),
+       ('bill_category', '5', '通讯', '账单分类'),
+       ('bill_category', '6', '学习', '账单分类'),
+       ('bill_category', '7', '住房', '账单分类'),
+       ('bill_category', '8', '购物', '账单分类'),
+       ('bill_category', '9', '其他', '账单分类');
